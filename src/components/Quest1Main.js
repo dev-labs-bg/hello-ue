805439803e186d52450a178c77774a7bbe1f35c9
DeskaD10 <128162312+DeskaD10@users.noreply.github.com>
2023-03-29 15:46:07 +0300

    updated quest1

--- src/components/Quest1Main.js
+++ src/components/Quest1Main.js
@@ -1,7 +1,6 @@
-//import { Image } from '@chakra-ui/react'
 import { Box, Heading, Button, Image } from '@chakra-ui/react'
 import kafeImg from '../Kafe.jpg'
-import '../style/quest1main.css'
+
 //import useAuth from '../../hooks/useAuth'
 
 const Quest1Main = () => {
@@ -10,17 +9,36 @@ const Quest1Main = () => {
 		return (
 
 
-			<Box  maxWidth={"75%"} margin={"0 auto"} boxShadow={"0px 2px 8px -1px"} marginTop={"4rem"} zIndex={"-1"} padding={"8rem"} pl={{ lg: '4em' }} pr={{ lg: '4em' }} bgColor="gray.100" position="relative">
+			<Box maxWidth={"75%"}
+				margin={"0 auto"}
+				boxShadow={"0px 2px 8px -1px"}
+				marginTop={"4rem"}
+				zIndex={"-1"}
+				padding={"8rem"}
+				pl={{ lg: '4em' }}
+				pr={{ lg: '4em' }}
+				bgColor="gray.100"
+				position="relative">
+
 			<Box maxWidth={"100%"}>
-			<Box position={"absolute"} bgColor={"blue"} height={"90px"} width={"90px"} borderRadius={"50%"} color={'white'} fontSize={"32px"}
+
+			<Box position={"absolute"}
+				bgColor={"blue.400"}
+				height={"90px"}
+				width={"90px"}
+				borderRadius={"70%"}
+				color={'white'}
+				fontSize={"32px"}
 			    display={"flex"}
 				justifyContent={"center"}
 				alignItems={"center"}
-				top={"7%"}
-				left={"-1px"}>
+				fontWeight={"bold"}
+				top={"6%"}
+				left={"5px"}>
 				1
 			</Box>
-				<Image className='snimka' src={kafeImg} alt={"Coffee"} />
+				<Image src={kafeImg} alt={"Coffee"} />
+
 				<Box
 					bgSize="cover"
 					bgPosition="top center"
@@ -39,7 +57,7 @@ const Quest1Main = () => {
 				</Box>
 				<Button
 					mt={10}
-					bgColor="blue.500"
+					bgColor="blue.400"
 					color="white"
 					borderRadius="lg"
 					fontSize="1.5em"
