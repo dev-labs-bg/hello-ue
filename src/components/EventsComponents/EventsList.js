13827751ee0e24bee53e67528299614f72323c0c
unknown <tmindizova@gmail.com>
2023-04-05 21:28:33 +0300

    feat: added calendar view and events filtering

--- src/components/EventsComponents/EventsList.js
+++ src/components/EventsComponents/EventsList.js
@@ -1,9 +1,28 @@
 import { Box } from '@chakra-ui/react'
 import EventsListItem from './EventsListItem'
 import EventsListEmpty from './EventsListEmpty'
+import React, { useState, useEffect } from 'react'
 
-const EventsList = ({ events, isLoading }) => {
-	const eventsItems = events.map((event) => <EventsListItem event={event} />)
+const EventsList = ({ events, isLoading, currentView, selectedDate }) => {
+	// const eventsItems = events.map((event) => <EventsListItem event={event} />)
+	const eventsPerMonth = events.filter(
+		(event) =>
+			new Date(event.start).getMonth() === currentView.getMonth() &&
+			new Date(event.start).getYear() === currentView.getYear()
+	)
+
+	const eventsItems = eventsPerMonth.map((event) => (
+		<EventsListItem event={event} />
+	))
+
+	const selectedEvent = events.find(
+		(event) =>
+			new Date(selectedDate).getDate() ===
+				new Date(event.start).getDate() &&
+			new Date(selectedDate).getMonth() ===
+				new Date(event.start).getMonth() &&
+			new Date(selectedDate).getYear() === new Date(event.start).getYear()
+	)
 
 	if (!isLoading && events.length === 0) {
 		return <EventsListEmpty />
@@ -16,7 +35,14 @@ const EventsList = ({ events, isLoading }) => {
 					alignItems: 'center',
 				}}
 			>
-				<Box>{eventsItems}</Box>
+				{selectedDate !== 0 && !selectedEvent && <EventsListEmpty />}
+				{selectedDate > 0 && selectedEvent && (
+					<EventsListItem event={selectedEvent} />
+				)}
+				{selectedDate === 0 && <Box>{eventsItems}</Box>}
+				{selectedDate === 0 && eventsItems.length === 0 && (
+					<EventsListEmpty />
+				)}
 			</Box>
 		)
 	}
--- src/components/EventsComponents/EventsMain.js
+++ src/components/EventsComponents/EventsMain.js
@@ -2,25 +2,56 @@ import React, { useState, useEffect } from 'react'
 import EventsError from './EventsError'
 import EventsList from './EventsList'
 import EventsLoading from './EventsLoading'
+import EventsCalendar from './EventsCalendar'
+import { Box } from '@chakra-ui/react'
 
 const EventsMain = () => {
-	useEffect(() => {
-		fetchEvents()
-	 }, [])
-
 	const [events, setEvents] = useState([])
 	const [isError, setError] = useState(false)
 	const [isLoading, setLoading] = useState(true)
+	const [currentView, setCurrentView] = useState(new Date())
+	const [selectedDate, setSelectedDate] = useState(0)
 
-	if (isError) {
-		return (
-			<EventsError/>
-		)
-	} else if (isLoading) {
-		return <EventsLoading/>
-	} else {
-		return <EventsList events={events} isLoading = {isLoading}/>
+	useEffect(() => {
+		fetchEvents()
+	}, [])
+
+	function onClickDate(value) {
+		setSelectedDate(value)
 	}
+
+	function onChangeView(activeStartDate) {
+		setCurrentView(activeStartDate)
+		setSelectedDate(0)
+	}
+
+	return (
+		<Box
+			display="flex"
+			alignItems="center"
+			flexDirection="column"
+			p="4"
+			minH="100vh"
+			bgColor="gray.100"
+		>
+			<EventsCalendar
+				events={events}
+				onClickDate={onClickDate}
+				onChangeView={onChangeView}
+			/>
+			{isError && <EventsError />}
+			{!isError && isLoading ? (
+				<EventsLoading />
+			) : (
+				<EventsList
+					events={events}
+					isLoading={isLoading}
+					currentView={currentView}
+					selectedDate={selectedDate}
+				/>
+			)}
+		</Box>
+	)
 
 	async function fetchEvents() {
 		let response = null
