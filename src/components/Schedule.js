03f4736673043762f7b570a9eafd3041018bbf8a
Alex Rashkov <alex011445@gmail.com>
2023-06-04 09:26:34 +0300

    Added screens for loading and no schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,15 +1,30 @@
 import Chart from './ChartComponents/Chart'
 import useScheduleData from '../hooks/useScheduleData'
 
+const loadingImage = process.env.PUBLIC_URL + '/loading.gif'
+const errorImage = process.env.PUBLIC_URL + '/no-access.gif'
+const errorMessage =
+	'Опа! Няма налични данни за графика. Моля опитайте покъсно.'
+
 const Schedule = () => {
 	const { chartData, isLoading, error } = useScheduleData()
 
 	return (
 		<div className="chart-container">
 			{isLoading ? (
-				<div>Loading...</div>
+				<div className="loading-container">
+					<img
+						src={loadingImage}
+						alt="Loading..."
+						className="loading-image"
+					/>
+					<div className="loading-text">Loading...</div>
+				</div>
 			) : error ? (
-				<div>Error: {error}</div>
+				<div className="error-container">
+					<img src={errorImage} alt="Error" className="error-image" />
+					<div className="error-message">{errorMessage}</div>
+				</div>
 			) : (
 				<Chart data={chartData} />
 			)}
